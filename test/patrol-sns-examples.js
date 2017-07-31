2b33b950436ca34fb4b435cf3abc38afba86e716
Kara Mahoney <kara.mahoney@mapbox.com>
2017-07-31 16:13:36 -0700

    added patrol sns message examples

---
+++ test/patrol-sns-examples.js
@@ -0,0 +1,40 @@
+module.exports.examples = {
+  mfaDisabled = {
+    'timestamp' : '2017-07-31T00:54:06.655Z',
+    'title': 'User feanor has disabled 2FA on their Github account',
+    'priority': 'self_service',
+    'username': {
+      'github': 'feanor'
+    },
+    'message': {
+      'github': {
+        'issue': 'Two factor authentication has been disabled...',
+        'directions': 'Ensure you re-enable 2FA on your Github account...'
+      },
+      'slack': {
+        'prompt': 'Did you turn off two-factor authentication...',
+        'response': ['Yes', 'No'],
+        'condition': 'Responding `Yes` will... Respoding `No` will...'
+      }
+    }
+  },
+  madePublic = {
+    'timestamp' : '2017-07-31T00:54:06.655Z',
+    'title': 'Prvate repository silmarils made public by feanor',
+    'priority': 'self_service',
+    'username': {
+      'github': 'feanor'
+    },
+    'message': {
+      'github': {
+        'issue': 'A private Mapbox repository has been made public...',
+        'directions': 'Please review the contents of this repository and...'
+      },
+      'slack': {
+        'prompt': 'Did you make this private repository public...',
+        'response': ['Yes', 'No'],
+        'condition': 'Responding `Yes` will... Respoding `No` will...'
+      }
+    }
+  }
+};
