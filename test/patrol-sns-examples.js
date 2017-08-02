43c33656ba3fbeb011ea05712aa78ec5fdd7c3f9
Kara Mahoney <kara.mahoney@mapbox.com>
2017-08-01 17:22:05 -0700

    refactor webhooks -> RTM bot

--- test/patrol-sns-examples.js
+++ test/patrol-sns-examples.js
@@ -3,22 +3,20 @@ module.exports.examples = {
     'timestamp' : '2017-07-31T00:54:06.655Z',
     'title': 'User feanor has disabled 2FA on their Github account',
     'priority': 'self_service',
-    'username': {
-      'github': 'feanor'
-    },
+    'username': 'feanor',
     'message': {
       'issue': 'Two factor authentication has been disabled...',
-      'directions': 'Ensure you re-enable 2FA on your Github account...'
+      'directions': 'Ensure you re-enable 2FA on your Github account...',
       'prompt': {
         'message': 'Did you turn off two-factor authentication on your GitHub account? ...',
-        'actions': {
+        'actions': [
           {
             'value': 'Yes'
           },
           {
             'value': 'No'
           }
-        }
+        ]
       }
     }
   },
@@ -26,22 +24,20 @@ module.exports.examples = {
     'timestamp' : '2017-07-31T00:54:06.655Z',
     'title': 'Prvate repository silmarils made public by feanor',
     'priority': 'self_service',
-    'username': {
-      'github': 'feanor'
-    },
+    'username': 'feanor',
     'message': {
       'issue': 'A private Mapbox repository has been made public...',
-      'directions': 'Please review the contents of this repository and...'
+      'directions': 'Please review the contents of this repository and...',
       'prompt': {
         'message': 'Did you make this private repository public? ...',
-        'actions': {
+        'actions': [
           {
             'value': 'Yes'
           },
           {
             'value': 'No'
           }
-        }
+        ]
       }
     }
   }
