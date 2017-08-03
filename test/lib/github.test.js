6ee5e1dcf67efb7cfbd24dd4e75527fa1721526c
Olivia Brundage <olivia.brundage@mapbox.com>
2017-08-03 13:46:08 -0700

    adding aut test

--- test/lib/github.test.js
+++ test/lib/github.test.js
@@ -1,20 +1,34 @@
 const tape = require('tape');
 const nock = require('nock');
-const githubAPI = require('github');
-const github = new githubAPI({});
-const githubRequests = require('../lib/github.js')
+const githubRequests = require('../../lib/github.js');
 
 // Auth into GH
+tape('Receive auth object from request', function(assert) {
+  let auth = {
+    type: 'oauth',
+    token: 'FakeApiToken'
+  };
 
-// Does the issue exist? (look at array)
-tape('Finds a match to the requested issue', function(assert) {
-
+  githubRequests.authenticate('FakeApiToken', function(err, res){
+    console.log('HI');
+    assert.ifErr(err, 'Function does not error');
+    assert.true(res.auth, auth, 'Function returns expected auth object');
+    assert.end();
+  });
 });
 
-tape('Does not find a match to the request issue', function(assert) {
-
-})
-
+// Does the issue exist? (look at array)
+// tape('Finds a match to the requested issue', function(assert) {
+//
+// });
+//
+// tape('Does not find a match to the request issue', function(assert) {
+//
+// });
+//
+// tape('Pagination', function(assert) {
+//
+// });
 
 // Create issue
   // Find Issue. Is a dub? Do a comment.
