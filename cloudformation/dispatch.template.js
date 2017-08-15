79d7ba6d7a361f89bcf4dfca0a67a7055371813b
Olivia Brundage <olivia.brundage@mapbox.com>
2017-08-15 13:24:02 -0400

    fixing renaming

--- cloudformation/dispatch.template.js
+++ cloudformation/dispatch.template.js
@@ -2,8 +2,8 @@ var lambdaCfn = require('lambda-cfn');
 
 module.exports = lambdaCfn(
   [
-    'rules/dispatch-incoming.js',
-    'rules/dispatch-triage.js'
+    'rules/incoming.js',
+    'rules/triage.js'
   ], {
     "AWSTemplateFormatVersion": "2010-09-09",
     "Description": "dispatch"
